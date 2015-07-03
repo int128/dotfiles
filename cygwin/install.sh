ca6d910c294367771b6b8f17808b4098a456fd31
int128 <int128@gmail.com>
2015-07-04 00:24:34 +0900

    Move cygwin specific files

--- cygwin/install.sh
+++ cygwin/install.sh
@@ -1,11 +1,17 @@
 #!/bin/bash -xe
 
+test "$(uname -o)" = "Cygwin"
+
+# Install dotfiles
+ln -snfv dotfiles/cygwin/.minttyrc "$HOME/.minttyrc"
+cp -av dotfiles/cygwin/_vimrc "$HOME/_vimrc"
+cp -av dotfiles/cygwin/_gvimrc "$HOME/_gvimrc"
+
 cygwin_base="$(dirname $0)"
 cd "$cygwin_base"
 
 # git-credential-winstore
 test -f "$HOME/AppData/Roaming/GitCredStore/git-credential-winstore.exe"
-git config --global core.autocrlf input
 git config --global credential.helper "$HOME/AppData/Roaming/GitCredStore/git-credential-winstore.exe"
 
 # apt-cyg
--- install.sh
+++ install.sh
@@ -1,54 +1,15 @@
-#!/bin/bash
+#!/bin/bash -xe
 
-dotfiles="$(dirname $0)"
-os="$(uname -o)"
+# Install dotfiles
+ln -snfv dotfiles/.zshrc "$HOME/.zshrc"
+ln -snfv dotfiles/.vimrc "$HOME/.vimrc"
+ln -snfv dotfiles/.gvimrc "$HOME/.gvimrc"
+ln -snfv dotfiles/.vim "$HOME/.vim"
 
-function install_link () {
-  local name="$1"
-  local dotfile="$dotfiles/$name"
-  local target="$HOME/$name"
-  if [ -e "$dotfile" ]; then
-    if [ ! -e "$target" ]; then
-      ln -sv "$dotfile" "$target"
-    elif [ -L "$target" ]; then
-      echo "Info: already installed: $target"
-    else
-      echo "Warning: should be symlink but not: $target"
-    fi
-  else
-    echo "Fatal: source does not exist: $dotfile"
-  fi
-}
-
-function install_copy () {
-  local name="$1"
-  local dotfile="$dotfiles/$name"
-  local target="$HOME/$name"
-  if [ -e "$dotfile" ]; then
-    if [ ! -e "$target" ]; then
-      cp -av "$dotfile" "$target"
-    elif [ -f "$target" ]; then
-      echo "Info: already installed: $target"
-    else
-      echo "Warning: should be file but not: $target"
-    fi
-  else
-    echo "Fatal: source does not exist: $dotfile"
-  fi
-}
-
-install_link .zshrc
-install_link .vim
-install_link .vimrc
-install_link .gvimrc
-
-if [ "$os" == "Cygwin" ]; then
-  install_link .minttyrc
-  install_copy _vimrc
-  install_copy _gvimrc
-fi
-
+# Fix permission for security
 chmod -v 700 "$HOME/dotfiles/.vim/tmp"
 
+# Git
 git config --global include.path "$HOME/dotfiles/.gitconfig"
+git config --global core.autocrlf input
 
