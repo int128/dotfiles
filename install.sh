1c37e96ad2d22c0774f20bb69f97d5693b2b64ef
int128 <int128@gmail.com>
2015-07-15 22:53:19 +0900

    Configure user name and email of Git

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -xe
+#!/bin/zsh -xe
 
 # Install dotfiles
 ln -snfv dotfiles/.zshrc "$HOME/.zshrc"
@@ -10,6 +10,9 @@ ln -snfv dotfiles/.vim "$HOME/.vim"
 chmod -v 700 "$HOME/dotfiles/.vim/tmp"
 
 # Git
+git_author=("${(f)$(git log -1 --format=format:%an%n%ae)}")
+git config --global user.name  "${git_author[1]}"
+git config --global user.email "${git_author[2]}"
 git config --global include.path "$HOME/dotfiles/.gitconfig"
 git config --global core.autocrlf input
 
