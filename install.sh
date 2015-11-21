2ede03e5ed241ba23dfd1a454364d64bf3fc2eef
int128 <int128@gmail.com>
2015-11-21 11:30:36 +0900

    Fix git configuration

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/sh -xe
 
+# Git
+git config --global user.name "`cd dotfiles && git log -1 --format=format:%an`"
+git config --global user.email "`cd dotfiles && git log -1 --format=format:%ae`"
+git config --global include.path "$HOME/dotfiles/.gitconfig"
+git config --global core.autocrlf input
+
 # Install dotfiles
 ln -snfv dotfiles/.zshrc "$HOME/.zshrc"
 ln -snfv dotfiles/.vimrc "$HOME/.vimrc"
@@ -10,12 +16,6 @@ ln -snfv dotfiles/.gemrc "$HOME/.gemrc"
 mkdir -v -m 700 "$HOME/.vim"
 mkdir -v -m 700 "$HOME/.vim/tmp"
 mkdir -v -m 700 "$HOME/.vim/bundle"
-
-# Git
-git config --global user.name "`git log -1 --format=format:%an`"
-git config --global user.email "`git log -1 --format=format:%ae`"
-git config --global include.path "$HOME/dotfiles/.gitconfig"
-git config --global core.autocrlf input
 
 # VIM
 git clone https://github.com/Shougo/neobundle.vim "$HOME/.vim/bundle/neobundle.vim"
