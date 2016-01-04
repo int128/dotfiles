6ea8119c627906dae5b8e8e3a5af8e46a20a79c1
int128 <int128@gmail.com>
2016-01-04 23:10:02 +0900

    Test on Travis CI

--- install.sh
+++ install.sh
@@ -1,21 +1,24 @@
-#!/bin/sh -xe
+#!/bin/bash -xe
+
+dotfiles_base="$(cd $(dirname "$0") && pwd)"
+dotfiles_relative_from_home="${dotfiles_base/$HOME\/}"
 
 # Git
-git config --global user.name "`cd dotfiles && git log -1 --format=format:%an`"
-git config --global user.email "`cd dotfiles && git log -1 --format=format:%ae`"
-git config --global include.path "$HOME/dotfiles/.gitconfig"
+git config --global user.name "$(cd "$dotfiles_base" && git log -1 --format=format:%an)"
+git config --global user.email "$(cd "$dotfiles_base" && git log -1 --format=format:%ae)"
+git config --global include.path "$dotfiles_base/.gitconfig"
 git config --global core.autocrlf input
 
 # Install dotfiles
-ln -snfv dotfiles/.zshrc "$HOME/.zshrc"
-ln -snfv dotfiles/.vimrc "$HOME/.vimrc"
-ln -snfv dotfiles/.gvimrc "$HOME/.gvimrc"
-ln -snfv dotfiles/.gemrc "$HOME/.gemrc"
+ln -snfv "$dotfiles_relative_from_home/.zshrc" "$HOME/.zshrc"
+ln -snfv "$dotfiles_relative_from_home/.vimrc" "$HOME/.vimrc"
+ln -snfv "$dotfiles_relative_from_home/.gvimrc" "$HOME/.gvimrc"
+ln -snfv "$dotfiles_relative_from_home/.gemrc" "$HOME/.gemrc"
 
 # Create dotvim
-mkdir -v -m 700 "$HOME/.vim"
-mkdir -v -m 700 "$HOME/.vim/tmp"
-mkdir -v -m 700 "$HOME/.vim/bundle"
+mkdir -p -v -m 700 "$HOME/.vim"
+mkdir -p -v -m 700 "$HOME/.vim/tmp"
+mkdir -p -v -m 700 "$HOME/.vim/bundle"
 
 # VIM
 git clone https://github.com/Shougo/neobundle.vim "$HOME/.vim/bundle/neobundle.vim"
