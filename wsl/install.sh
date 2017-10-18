d60b29bc45169e83555c18934590e5909ae68aa5
Hidetake Iwata <int128@gmail.com>
2017-10-18 15:49:13 +0900

    Add Google Cloud SDK

--- wsl/install.sh
+++ wsl/install.sh
@@ -6,3 +6,10 @@ if ! grep 'DISPLAY=' "$HOME/.zshrc.local"; then
   echo 'DISPLAY=:0.0' >> "$HOME/.zshrc.local"
 fi
 
+if ! gcloud; then
+  export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+  echo "deb https://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+  curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+  sudo apt-get update && sudo apt-get install google-cloud-sdk
+fi
+
