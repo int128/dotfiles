e3e7022fb668856eb50b77b7f07d4d1a8602a3ee
Hidetake Iwata <int128@gmail.com>
2017-11-09 16:51:55 +0900

    Add docker for WSL

--- wsl/install.sh
+++ wsl/install.sh
@@ -3,7 +3,11 @@
 sudo apt install x11-apps openjdk-8-jdk
 
 if ! grep 'DISPLAY=' "$HOME/.zshrc.local"; then
-  echo 'DISPLAY=:0.0' >> "$HOME/.zshrc.local"
+  echo 'export DISPLAY=:0.0' >> "$HOME/.zshrc.local"
+fi
+
+if ! grep 'DOCKER_HOST=' "$HOME/.zshrc.local"; then
+  echo 'export DOCKER_HOST=tcp://0.0.0.0:2375' >> "$HOME/.zshrc.local"
 fi
 
 if ! gcloud; then
@@ -12,4 +16,11 @@ if ! gcloud; then
   curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
   sudo apt-get update && sudo apt-get install google-cloud-sdk
 fi
+gcloud components list
+
+if ! docker-compose; then
+  sudo curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+fi
+docker-compose --version
 
