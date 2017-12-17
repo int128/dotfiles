b1515840efa64482eb1fe00a506e551b7131f10c
Hidetake Iwata <int128@gmail.com>
2017-12-17 16:54:03 +0900

    Fix apps

--- macos/install-brew-cask.sh
+++ macos/install-brew-cask.sh
@@ -5,26 +5,20 @@ brew tap caskroom/versions
 
 brew cask update
 
-brew cask install google-chrome
-brew cask install mplayerx
-brew cask install picasa
-brew cask install gimp
-
-brew cask install evernote
-brew cask install slack
-
 brew cask install iterm2
-brew cask install atom
-brew cask install gitx
-brew cask install macvim-kaoriya
-
-brew cask install tunnelblick
 brew cask install virtualbox
 brew cask install docker
 
+brew cask install google-chrome
+brew cask install atok
+brew cask install mplayerx
+brew cask install gimp
+brew cask install evernote
+brew cask install slack
+
+brew cask install visual-studio-code
+brew cask install atom
+brew cask install macvim-kaoriya
 brew cask install java
 brew cask install intellij-idea
-brew cask install android-studio
-
-brew cask install apache-directory-studio
 
