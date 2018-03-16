86402ca6146122314cdf2ed5a26b147b1bb674dc
Hidetake Iwata <int128@gmail.com>
2018-03-16 12:57:44 +0900

    Add WSL procedure

--- wsl/README.md
+++ wsl/README.md
@@ -1,11 +1,22 @@
 # Install on WSL
 
 ```sh
+umask 022
+git clone https://github.com/int128/dotfiles
+
 ./install.sh
+./wsl/install.sh
 ```
 
-Create a shortcut to open xterm.
+## xterm
+
+Install the following:
+
+- [VcXsrv](https://sourceforge.net/projects/vcxsrv/)
+
+Create a shortcut to open a xterm.
 
 ```
 C:\Windows\System32\wsl.exe DISPLAY=:0 xterm -e zsh -l
 ```
+
--- wsl/install.sh
+++ wsl/install.sh
@@ -1,26 +1,41 @@
 #!/bin/bash -xe
 
-sudo apt install x11-apps openjdk-8-jdk
+DOCKER_COMPOSE_VERSION=1.17.0
 
+sudo apt install zsh x11-apps xterm openjdk-8-jdk fonts-mplus
+
+# Store Git credentials
+git config --global credential.helper store
+
+# X server
 if ! grep 'DISPLAY=' "$HOME/.zshrc.local"; then
   echo 'export DISPLAY=:0.0' >> "$HOME/.zshrc.local"
 fi
 
+# Use remote Docker on Windows
 if ! grep 'DOCKER_HOST=' "$HOME/.zshrc.local"; then
   echo 'export DOCKER_HOST=tcp://0.0.0.0:2375' >> "$HOME/.zshrc.local"
 fi
 
-if ! gcloud; then
-  export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+# Google Cloud
+if [ ! -f /usr/bin/gcloud ]; then
+  CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
   echo "deb https://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
   curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
   sudo apt-get update && sudo apt-get install google-cloud-sdk
 fi
 gcloud components list
 
-if ! docker-compose; then
-  sudo curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+# Docker Compose
+if [ ! -f /usr/local/bin/docker-compose ]; then
+  sudo curl -L "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-`uname -s`-`uname -m`" -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
 fi
 docker-compose --version
+
+# Fonts
+if [ ! -d ~/.fonts ]; then
+  git clone --depth=1 https://github.com/hbin/top-programming-fonts ~/.fonts
+  fc-cache -f
+fi
 
