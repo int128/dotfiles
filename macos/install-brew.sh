c8f5db8035d984201efbcc074361704a3f38e879
Hidetake Iwata <int128@gmail.com>
2018-10-29 10:03:00 +0900

    Update packages

--- macos/install-brew.sh
+++ macos/install-brew.sh
@@ -8,13 +8,11 @@ brew cask install iterm2
 brew cask install google-chrome
 brew cask install atok
 brew cask install mplayerx
-brew cask install gimp
 brew cask install evernote
 brew cask install slack
 
 brew cask install visual-studio-code
 brew cask install atom
-brew cask install java
 brew cask install intellij-idea
 
 brew cask install google-cloud-sdk
