505f868eb6f25b65d8606eece1bd8e7c854256fc
Hidetake Iwata <int128@gmail.com>
2019-08-08 16:47:57 +0900

    Fix homebrew installation script

--- macos/install.sh
+++ macos/install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -xe
 
-[ ! -d "$HOME/.homebrew" ] && git clone --depth=1 https://github.com/Homebrew/homebrew.git "$HOME/.homebrew"
+[ ! -d "$HOME/.homebrew" ] && mkdir "$HOME/.homebrew" && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C "$HOME/.homebrew"
 
