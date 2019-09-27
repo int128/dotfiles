0c614dae2a77ed1de74e387dfbff619af30297bf
Hidetake Iwata <int128@gmail.com>
2019-09-27 10:25:43 +0900

    Update Homebrew packages

--- macos/install-brew.sh
+++ macos/install-brew.sh
@@ -1,21 +1,31 @@
 #!/bin/sh -xe
 
-brew tap caskroom/cask
-brew tap caskroom/versions
-
 brew cask install iterm2
 
+# Desktop
 brew cask install google-chrome
 brew cask install mplayerx
 brew cask install evernote
 brew cask install slack
 
+# Development
+brew install go
+brew install golangci-lint
+brew install node
+brew install yarn
+
 brew cask install visual-studio-code
 brew cask install atom
 brew cask install intellij-idea
 brew cask install goland
+brew cask install java
 
+# Infrastructure
+brew install awscli
+brew install terraform
+brew install helmfile
+brew install kops
+
+brew cask install docker
 brew cask install google-cloud-sdk
 brew cask install virtualbox
-brew cask install docker
-
--- macos/install.sh
+++ macos/install.sh
@@ -1,6 +1,11 @@
 #!/bin/sh -xe
 
-if [ ! -d "$HOME/.homebrew" ]; then
+homebrew_dir="$HOME/.homebrew"
+
+if [ ! -d "$homebrew_dir" ]; then
     # https://docs.brew.sh/Installation
-    mkdir "$HOME/.homebrew" && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C "$HOME/.homebrew"
+    mkdir "$homebrew_dir" && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C "$homebrew_dir"
 fi
+
+"$homebrew_dir/bin/brew" update
+"$homebrew_dir/bin/brew" tap caskroom/cask
