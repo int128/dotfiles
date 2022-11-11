00daaa7a8ae05135c061c4ebd20638fd7aae36de
Hidetake Iwata <int128@gmail.com>
2022-11-11 11:13:37 +0900

    Try bash

--- .github/screenshot.sh
+++ .github/screenshot.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# https://stackoverflow.com/questions/13762280/zsh-compinit-insecure-directories
-sudo chmod -R 755 /usr/local/share/zsh/site-functions
-sudo chown -R root:root /usr/local/share/zsh/site-functions
-
-export SHELL=/bin/zsh
+#export SHELL=/bin/zsh
 xterm &
 sleep 3
 import -window root screenshot.jpg
